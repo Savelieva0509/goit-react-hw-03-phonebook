a1c534db4b2922a30c085c1275e655b21502432a
Babenko Liudmyla <savelieva0509@gmail.com>
2022-12-09 17:52:10 +0200

    добавление иконки на кнопку

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,17 @@ import ContactList from './ContactList/ContactList';
 import shortid from 'shortid';
 
 export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  static defaultProps = {
+    initialContacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    ]
+  }
+
+  state = {
+    contacts: [],
     filter: '',
   };
 
@@ -21,7 +25,10 @@ export class App extends Component {
 
     if (parsedContacts) {
       this.setState({ contacts: parsedContacts });
-    }
+     }
+    else {
+      this.setState (this.props.initialContacts)
+     }
   }
 
   componentDidUpdate(prevProps, prevState) {
--- src/components/ContactItem/ContactItem.js
+++ src/components/ContactItem/ContactItem.js
@@ -1,18 +1,30 @@
+import IconButton from 'components/IconButton/IconButton';
 import PropTypes from 'prop-types';
 import css from './ContactItem.module.css';
+import { ReactComponent as DeleteIcon } from '../icons/delete.svg';
 
 const ContactItem = ({ id, name, number, onDeleteContact }) => (
   <li key={id} className={css.contactsList__item}>
     <p>
       {name}: {number}
     </p>
-    <button
+    
+    <IconButton
+      className={css.contactsList__button}
+      onClick={onDeleteContact}>
+      <DeleteIcon
+        className={css.deleteIcon}
+        width="20" height="20"
+        fill="#fff"/>
+        Delete
+    </IconButton>
+    {/* <button
       className={css.contactsList__button}
       type="button"
       onClick={onDeleteContact}
     >
-      Delete
-    </button>
+      
+    </button> */}
   </li>
 );
 
